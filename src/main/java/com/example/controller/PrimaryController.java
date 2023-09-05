aecbf0e65b3f5de901d522533caf2a4258a9efef
mthaugusto <mthaugusto94@icloud.com>
2023-09-05 17:51:50 -0300

    Primeiras alterações conexão com banco de dados

--- src/main/java/com/example/controller/PrimaryController.java
+++ src/main/java/com/example/controller/PrimaryController.java
@@ -1,8 +1,8 @@
 package com.example.controller;
 
-import java.util.ArrayList;
-
+import java.sql.SQLException;
 import java.util.Random;
+import com.example.data.ResultadoDao;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 
 public class PrimaryController {
-    // textField e TextArea são objetos/classes
     @FXML
     RadioButton rdbPedra;
     @FXML
@@ -25,6 +24,7 @@ public class PrimaryController {
 
     Integer ptsJogador = 0;
     Integer ptsBot = 0;
+    Integer numeroPartida = 0;
 
     Random numeroAleatorio = new Random();
 
@@ -54,7 +54,24 @@ public class PrimaryController {
         return opcaoJogadaBOT;
     }
 
+    public String determinarVencedor() {
+        if (ptsJogador == 3) {
+            return "Jogador";
+        } else if (ptsBot == 3) {
+            return "Bot";
+        }
+        return "Partida ainda não terminou";
+    }
+
     public void terminarPartida() {
+        ResultadoDao resultadoDao = new ResultadoDao();
+        String vencedor = determinarVencedor();
+        try {
+            resultadoDao.inserir(numeroPartida, ptsJogador, ptsBot, vencedor);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         if (ptsJogador == 3 || ptsBot == 3) {
             ptsJogador = 0;
             ptsBot = 0;
@@ -62,7 +79,6 @@ public class PrimaryController {
     }
 
     public void jogar() {
-
         capturarOpcao();
         definirJogadaBOT();
 
@@ -70,11 +86,9 @@ public class PrimaryController {
             Alert alertaEmpate = new Alert(AlertType.INFORMATION, "Empate!");
             alertaEmpate.setHeaderText("Resultado");
             alertaEmpate.show();
-            // return "Empate";
-        } else if ((opcaoSelecionada.equals("pedra") && opcaoJogadaBOT.equals("tesoura")) ||
-                (opcaoSelecionada.equals("papel") && opcaoJogadaBOT.equals("pedra")) ||
-                (opcaoSelecionada.equals("tesoura") && opcaoJogadaBOT.equals("papel"))) {
-
+        } else if ((opcaoSelecionada.equals("pedra") && opcaoJogadaBOT.equals("tesoura"))
+                || (opcaoSelecionada.equals("papel") && opcaoJogadaBOT.equals("pedra"))
+                || (opcaoSelecionada.equals("tesoura") && opcaoJogadaBOT.equals("papel"))) {
             ptsJogador++;
             if (ptsJogador < 3) {
                 Alert alertaVitoria = new Alert(AlertType.INFORMATION,
@@ -104,8 +118,7 @@ public class PrimaryController {
             }
         }
 
+        numeroPartida++;
         terminarPartida();
-
     }
-
 }
